33d0020123d0bdc47e188be31b77c83a6eef9252
Hasan Mukhlis <hsnmkls@proton.me>
2023-11-30 01:34:41 +0800

    game on 🎲

---
+++ src/Fortunes.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Fortunes {
+    /**
+		# Methods that we need:
+
+		## Deposits and withdrawals
+		1. Deposit at any time
+		2. Forfeit before game ends
+		3. Redeem at end of game
+
+		## User Game functions
+		1. Add
+		2. Multiply
+		3. Seize
+
+		## Admin Game functions
+		1. Start game
+		2. End game
+		3. Start seizure period
+		4. End seizure period
+
+		# Bookkeeping
+		1. Fortunes - a mapping of addresses to their fortunes.
+		2. Deposits - a mapping of addresses to their deposits.
+		3. Bank - fortunes lost by players accumulate here.
+	 */
+}
