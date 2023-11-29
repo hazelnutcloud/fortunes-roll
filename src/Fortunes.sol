71cde0d23a21e4e1e508d3654b3f7302ff6ba54c
Hasan Mukhlis <hsnmkls@proton.me>
2023-11-30 03:37:33 +0800

    implement more methods

--- src/Fortunes.sol
+++ src/Fortunes.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {ERC721} from "solmate/tokens/ERC721.sol";
+import {ReentrancyGuard} from "solmate/utils/ReentrancyGuard.sol";
 import {Owned} from "solmate/auth/Owned.sol";
+import {VRFConsumerBaseV2} from "./chainlink/VRFConsumerBaseV2.sol";
 
-contract Fortunes is ERC721, Owned {
+contract Fortunes is VRFConsumerBaseV2, Owned, ReentrancyGuard {
     /**
 		# Methods that we need:
 
@@ -14,19 +15,22 @@ contract Fortunes is ERC721, Owned {
 		3. Redeem at end of game
 
 		## User Game functions
-		1. Add
-		2. Multiply
-		3. Seize
+		1. Roll For Add
+		2. Roll For Multiply
+		3. Roll For Seizure
+
+		## Chainlink VRF functions
+		1. FulfillRandomness - generates a new dice roll for a user
 
 		## Admin Game functions
-		1. Start game
+		1. Start game - sets game parameters: dice roll generation rate, addition multiplier, multiplication multiplier
 		2. End game
-		3. Set seziure parameters
+		3. Set open seizure
 
 		# Bookkeeping
-		1. Fortunes - a mapping of tokenIds to their fortunes.
-		2. Deposits - a mapping of tokenIds to their deposits.
-		3. Bank - fortunes lost by players accumulate here.
+		1. Fortunes - a mapping of addresses to their fortunes.
+		2. Deposits - a mapping of addresses to their deposits.
+		3. Vault - fortunes lost by players accumulate here.
 
 		# Parameters
 		1. Dice roll generation rate - how often to generate a new dice roll per deposit.
@@ -34,85 +38,111 @@ contract Fortunes is ERC721, Owned {
 		3. Multiplication multiplier - how much to multiply the dice roll by when multiplying.
 	 */
 
-    struct SeizurePeriod {
+    struct FortuneSeeker {
+        uint256 fortune;
+        uint256 deposit;
+        uint256 diceRollsRemaining;
+        uint256 lastDiceRollTimestamp;
+    }
+
+    struct Seizure {
         uint256 start;
         uint256 end;
-        mapping(uint8 => uint256) rewards;
+        uint256[12] rewards;
     }
-    SeizurePeriod public seizure;
 
-    mapping(uint256 => uint256) public fortunes;
-    mapping(uint256 => uint256) public deposits;
+		enum RollAction {
+			Add,
+			Multiply,
+			Seizure
+		}
+
+    uint256 private constant PRECISION = 1e6;
 
     uint256 public gameStart;
     uint256 public gameEnd;
-    uint256 public bank;
+    uint256 public vault;
+    uint256 public totalFortune;
+    uint256 public totalDepositors;
 
-    constructor() ERC721("Fortunes", "FORTUNE") {}
+    uint256 public diceRollGenerationRate;
+    uint256 public additionMultiplier;
+    uint256 public multiplicationMultiplier;
 
-    function deposit(uint256 tokenId) external payable {
+    Seizure public openSeizure;
+
+    mapping(address => FortuneSeeker) public fortuneSeekers;
+
+    constructor(
+        address owner,
+        address vrfCoordinator
+    ) VRFConsumerBaseV2(vrfCoordinator) Owned(owner) {}
+
+    function deposit() external payable {
         require(msg.value > 0, "Must deposit more than 0");
-        deposits[msg.sender] += msg.value;
+
+        FortuneSeeker storage fortuneSeeker = fortuneSeekers[msg.sender];
+
+        if (fortuneSeeker.deposit == 0) {
+            totalDepositors += 1;
+        }
+
+        fortuneSeeker.deposit += msg.value;
     }
 
-    function forfeit(uint256 tokenId) external {
-        require(deposits[msg.sender] > 0, "Must have a deposit");
-        uint256 deposit = deposits[msg.sender];
-        deposits[msg.sender] = 0;
-        payable(msg.sender).transfer(deposit);
+    function forfeit() external nonReentrant {
+        FortuneSeeker storage fortuneSeeker = fortuneSeekers[msg.sender];
+
+        require(fortuneSeeker.deposit > 0, "Must have a deposit");
+        require(
+            block.timestamp >= gameStart && block.timestamp <= gameEnd,
+            "Must be during game"
+        );
+
+        uint256 deposited = fortuneSeeker.deposit;
+
+        delete fortuneSeekers[msg.sender];
+        totalDepositors -= 1;
+        vault += deposited;
+
+        payable(msg.sender).transfer(deposited);
     }
 
-    function redeem(uint256 tokenId) external {
-        require(fortunes[msg.sender] > 0, "Must have a fortune");
-        uint256 fortune = fortunes[msg.sender];
-        fortunes[msg.sender] = 0;
-        payable(msg.sender).transfer(fortune);
+    function redeem() external nonReentrant {
+        FortuneSeeker storage fortuneSeeker = fortuneSeekers[msg.sender];
+
+        require(fortuneSeeker.deposit > 0, "Must have a fortune");
+        require(block.timestamp >= gameEnd, "Must be after game has ended");
+
+        uint256 rewards = calculateRewards(fortuneSeeker);
+        uint256 redemption = rewards + fortuneSeeker.deposit;
+
+        totalDepositors -= 1;
+        totalFortune -= fortuneSeeker.fortune;
+
+        delete fortuneSeekers[msg.sender];
+
+        payable(msg.sender).transfer(redemption);
     }
 
-    function add(uint256 tokenId) external {
-        require(fortunes[msg.sender] > 0, "Must have a fortune");
-        fortunes[msg.sender] += a + b;
+    function calculateRewards(
+        FortuneSeeker storage fortuneSeeker
+    ) internal view returns (uint256) {
+        return (address(this).balance * fortuneSeeker.fortune) / totalFortune;
     }
 
-    function multiply(uint256 tokenId) external {
-        require(fortunes[msg.sender] > 0, "Must have a fortune");
-        fortunes[msg.sender] += a * b;
+    function rollDice(RollAction action) external nonReentrant {
+        FortuneSeeker storage fortuneSeeker = fortuneSeekers[msg.sender];
+
+        require(fortuneSeeker.deposit > 0, "Must have a deposit");
+        require(
+            block.timestamp >= gameStart && block.timestamp <= gameEnd,
+            "Must be during game"
+        );
     }
 
-    function seize(address user) external {
-        require(fortunes[user] > 0, "Must have a fortune");
-        uint256 fortune = fortunes[user];
-        fortunes[user] = 0;
-        bank += fortune;
-    }
-
-    function startGame() external {
-        require(gameStart == 0, "Game already started");
-        gameStart = block.timestamp;
-        gameEnd = block.timestamp + 1 days;
-    }
-
-    function endGame() external {
-        require(gameStart > 0, "Game not started");
-        require(block.timestamp > gameEnd, "Game not ended");
-        gameStart = 0;
-        gameEnd = 0;
-    }
-
-    function startSeizure() external {
-        require(gameStart > 0, "Game not started");
-        require(block.timestamp > gameEnd, "Game not ended");
-        require(seizureStart == 0, "Seizure already started");
-        seizureStart = block.timestamp;
-        seizureEnd = block.timestamp + 1 days;
-    }
-
-    function endSeizure() external {
-        require(gameStart > 0, "Game not started");
-        require(block.timestamp > gameEnd, "Game not ended");
-        require(seizureStart > 0, "Seizure not started");
-        require(block.timestamp > seizureEnd, "Seizure not ended");
-        seizureStart = 0;
-        seizureEnd = 0;
-    }
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
 }
